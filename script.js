187c1bc490f3e629abcceb610f2dd5bf31f7c583
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2022-11-20 15:44:17 -0500

    added all popups

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 // Assignment Code
 var create = document.querySelector("#generate");
-var characterAmount = 8-128;
-var addNumber = true;
+var characterAmount = 10;
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  //  var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+   passwordText.value = password;
 
   var input = window.prompt("how many characters do you want in your password? (8-128)");
-  if (input != characterAmount){
-    window.prompt("would you like to add a number?")
+  console.log(input);
 
-  if (addNumber = true){
-    window.prompt("would you like to add special charaters?")
-  }
+  if (input = characterAmount){
+      var upperCase = window.prompt("would you like to have an upper case letter?")
+  };
+  console.log (upperCase);
 
-  } 
+  if (upperCase = true){
+    var numbers = window.prompt("would you like to add any numbers?")
+  };
+  console.log(numbers);
 
+  if (numbers = true){
+    var spec= window.prompt("would you like to include any special characters?")
+  };
+  console.log(spec);
+
+
+window.alert("your new password!" + password)
+
+};
  
 
-}
 
 // Add event listener to generate button
 create.addEventListener("click", writePassword);
