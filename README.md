541a10a2796af2504319ed13efccac331c7c1c30
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2022-11-20 14:14:04 -0500

    base funtional JS for window prompts

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# password-generator-webpage
-creating a password generator using JavaScript
+# Password Generator!
+## This will be my first webpage using JavaScript. When the generate password button is clicked a series a prompt will show up asking for input on password criteria. Then you be given a password matching said criteria!
+
+# Technologies used:
+## HTML, CSS, and JavaScript.
+
+# The Webpage:
+## Screenshot:
+
+## Live URL:
--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var create = document.querySelector("#generate");
+var characterAmount = 8-128;
+var addNumber = true;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  var input = window.prompt("how many characters do you want in your password? (8-128)");
+  if (input != characterAmount){
+    window.prompt("would you like to add a number?")
+
+  if (addNumber = true){
+    window.prompt("would you like to add special charaters?")
+  }
+
+  } 
+
+ 
+
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+create.addEventListener("click", writePassword);
