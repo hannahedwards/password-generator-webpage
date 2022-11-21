8ea895ba0ae37967180e077a660e289bdfdbef86
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2022-11-20 19:50:11 -0500

    updated js

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 // Assignment Code
 var create = document.querySelector("#generate");
-var characterAmount = 10;
+var characterAmount = 8
 
 // Write password to the #password input
 function writePassword() {
-  //  var password = generatePassword();
    var passwordText = document.querySelector("#password");
    passwordText.value = password;
 
   var input = window.prompt("how many characters do you want in your password? (8-128)");
   console.log(input);
+  if (!input){
+    return;
+  }
 
-  if (input = characterAmount){
+  else if (input != characterAmount){
       var upperCase = window.prompt("would you like to have an upper case letter?")
   };
   console.log (upperCase);
+  if (!upperCase){
+    return;
+  }
 
-  if (upperCase = true){
+  else if (upperCase = true){
     var numbers = window.prompt("would you like to add any numbers?")
   };
   console.log(numbers);
+  if (!numbers){
+    return;
+  }
 
-  if (numbers = true){
+  else if (numbers = true){
     var spec= window.prompt("would you like to include any special characters?")
   };
   console.log(spec);
+  if (!spec){
+    return;
+  }
 
-
-window.alert("your new password!" + password)
 
 };
  
